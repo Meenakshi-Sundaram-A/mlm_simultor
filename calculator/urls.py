61bf33f6a9496413bd6690f6f900f920abb13592
sundar <sundara8558@gmail.com>
2024-11-13 20:07:10 +0530

    Added API to send and recieve data from Go

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
-# urlpatterns = [
-    
-# ]
+urlpatterns = [
+    path('',views.user_input_view,name='user_input'),
+    path('process_results/',views.process_results,name='process_results'),
+]
--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+import json
+import requests
 from .forms import FormData
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def user_input_view(request):
@@ -29,7 +34,45 @@ def user_input_view(request):
                 "carry_yes_no":carry_yes_no
             }
             
-            return render(request, 'success.html', {'data': form.cleaned_data})
+            try:
+                response = requests.post('http://localhost:8080/api/processData', json=data)
+                response.raise_for_status()
+                results = response.json()
+                return render(request, 'display_members.html', {
+                    'results': results,
+                })
+            except requests.exceptions.RequestException as e:
+                return JsonResponse({'error': f'Failed to communicate with Go server: {str(e)}'}, status=500)
         else:
-            # If the form is not valid, return the form with errors
             return render(request, 'form_template.html', {'form': form})
+            
+    else:
+        form = FormData()
+        return render(request, 'form_template.html', {'form': form})
+    
+@csrf_exempt
+def process_results(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
+        sponsor_bonus = data.get('total_sponsor_bonus', 0.0)
+        binary_bonus = data.get('total_binary_bonus', 0.0)
+        nodes = data.get('tree_structure', [])
+
+        context = {
+            'sponsor_bonus': sponsor_bonus,
+            'binary_bonus': binary_bonus,
+            'nodes': nodes
+        }
+
+        try:
+            render_context = render(request, 'display_members.html', context)
+            return render_context
+        except Exception as e:
+            return JsonResponse({'error': 'Template rendering error'}, status=500)
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
