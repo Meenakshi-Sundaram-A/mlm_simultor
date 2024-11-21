19cd22fe3520c6aae477bf9a536e1feccbae7c29
sundar <sundara8558@gmail.com>
2024-11-21 20:16:07 +0530

    Completed Binary and Unilevel simulation

--- calculator/forms.py
+++ calculator/forms.py
@@ -39,12 +39,19 @@ class FormData(forms.Form):
     
 class UniLevelFormData(forms.Form):
     num_of_users = forms.IntegerField(label="Enter the number of Users",required=True)
-    package_price = forms.FloatField(label="Enter the price of the package",required=True)
+    max_child = forms.IntegerField(label="Enter max children per person", required=True)
+    product_price = forms.CharField(
+        label="Enter Product Price (eg: 50,25,10)",
+        required=True,
+    )
+    users_per_product = forms.CharField(
+        label="Enter No of User per Product (eg: 50,25,10)",
+        required=True,
+    )
     sponsor_bonus_percentage = forms.IntegerField(label="Enter Sponsor Bonus Percentage")
     matching_bonus_percentages = forms.CharField(
         label="Enter Matching Bonus Percentages (eg: 50,25,10)",
         required=True,
     )
-    max_child = forms.IntegerField(label="Enter max children per person", required=True)
     capping_amount = forms.IntegerField(label="Enter the Capping Amount")
     
--- calculator/views.py
+++ calculator/views.py
@@ -41,15 +41,26 @@ def user_input_view(request):
                     "plan_type":"binary",
                 }
         elif plan_type == "unilevel" and unilevel_form.is_valid():
+            
             percentages_str = unilevel_form.cleaned_data['matching_bonus_percentages']
             matching_bonus_percentages = [int(x.strip()) for x in percentages_str.split(",")]
+            
+            product_price_str = unilevel_form.cleaned_data['product_price']
+            product_price = [int(x.strip()) for x in product_price_str.split(",")]
+            
+            users_per_product_str = unilevel_form.cleaned_data['users_per_product']
+            users_per_product = [int(x.strip()) for x in users_per_product_str.split(",")]
+            cycle = math.ceil(unilevel_form.cleaned_data['num_of_users'] / (sum(users_per_product)))
+            
             data = {
                     "num_of_users": unilevel_form.cleaned_data['num_of_users'],
-                    "package_price":unilevel_form.cleaned_data['package_price'],
+                    "product_price":product_price,
+                    "users_per_product":users_per_product,
                     "sponsor_bonus_percentage":unilevel_form.cleaned_data['sponsor_bonus_percentage'],
                     "percentage_string":matching_bonus_percentages,
                     "max_child":unilevel_form.cleaned_data['max_child'],
                     "capping_amount":unilevel_form.cleaned_data['capping_amount'],
+                    "cycle":cycle,
                     "plan_type":"unilevel",
                 }
             
@@ -64,7 +75,7 @@ def user_input_view(request):
             response = requests.post('http://localhost:8080/api/processData', json=data)
             response.raise_for_status()
             results = response.json()
-            return render(request, 'display_members.html', {'results': results})
+            return render(request, 'display_members.html', {'all_results': results})
         except requests.exceptions.RequestException as e:
             return JsonResponse({'error': f'Failed to communicate with Go server: {str(e)}'}, status=500)
             
